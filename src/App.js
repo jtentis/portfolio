d4a6776a579b3499faa75e96bcd080740eb5d4bd
Joao Tentis <101594270+jtentis@users.noreply.github.com>
2024-02-06 10:58:16 -0300

    apenas testando conhecimento

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+let string = "arroz";
+
 function App() {
-  return (
+  const teste = (valor) => (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Segundo testeasdas.</p>
+        <p>{valor}</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://localhost/moodle"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Moodle
         </a>
       </header>
     </div>
   );
+  return teste(string);
 }
 
 export default App;
